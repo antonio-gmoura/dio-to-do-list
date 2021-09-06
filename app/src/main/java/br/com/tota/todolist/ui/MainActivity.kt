5d5fe811a81d417307ff375e9fd2cb91a8d3792f
Antonio Moura <antonio.gmoura@gmail.com>
2021-09-05 20:55:56 -0400

    Aula 12 - Finalizando o app

--- app/src/main/java/br/com/tota/todolist/ui/MainActivity.kt
+++ app/src/main/java/br/com/tota/todolist/ui/MainActivity.kt
@@ -5,6 +5,7 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.View
 import br.com.tota.todolist.databinding.ActivityMainBinding
 import br.com.tota.todolist.datasource.TaskDataSource
 
@@ -48,7 +49,11 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun updateList() {
-        adapter.submitList(TaskDataSource.getList())
+        val list = TaskDataSource.getList()
+        binding.includeEmpty.emptyState.visibility = if (list.isEmpty()) View.VISIBLE
+        else View.GONE
+
+        adapter.submitList(list)
     }
 
     companion object {
